f57e7f145e700407f3a4cc7672d62de0dc9681ac
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2018-09-17 13:04:55 +0530

    Initial upload

---
+++ weathre-app/src/components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Form = props => {
+	return (
+		<form onSubmit={props.getWeather}>
+			<input type="text" name="city" placeholder="City..." />
+			<input type="text" name="Country" placeholder="Country..." />
+			<button>Get Weather</button>
+		</form>
+	);
+};
+
+export default Form;
---
+++ weathre-app/src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Titles = () => (
+	<div>
+		<h1 className="title-container__title">Whether Fineder</h1>
+		<p className="title-container__subtitle">Findout Temperature Conditions And More...</p>
+	</div>
+);
+
+export default Titles;
